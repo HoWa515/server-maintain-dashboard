056d72aa15c0c137ebea6ec296956df8e78f51aa
wh <sunfortea123@gmail.com>
2025-07-18 12:20:18 +0200

    first commit

---
+++ src/app/shared/control/control.component.ts
@@ -0,0 +1,26 @@
+import {
+  Component,
+  ElementRef,
+  inject,
+  Input,
+  ViewEncapsulation,
+} from '@angular/core';
+
+@Component({
+  selector: 'app-control',
+  standalone: true,
+  imports: [],
+  templateUrl: './control.component.html',
+  styleUrl: './control.component.css',
+  encapsulation: ViewEncapsulation.None,
+  host: { class: 'control', '(click)': 'onClick()' },
+})
+export class ControlComponent {
+  @Input({ required: true }) label!: string;
+  private hostEl = inject(ElementRef);
+
+  onClick() {
+    console.log('🧡');
+    console.log(this.hostEl);
+  }
+}
